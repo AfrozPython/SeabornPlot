923d4d240116ccd63ad30772738c82d4ab6b2a6a
Afroz <109797079+AfrozPython@users.noreply.github.com>
2023-01-03 03:21:59 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan  3 00:55:10 2023
+
+@author: Afroz
+"""
+
+import markdown
+import pandas as pd
+import streamlit as st
+import matplotlib.pyplot as plt 
+import seaborn as sns
+
+
+st.title("Iris Flower Classifier") 
+st.markdown('Use this Streamlit app to make your own scatterplot about Iris Flower') 
+ 
+selected_x_var = st.selectbox('What do want the x variable to be?', 
+  ['SepalLengthCm', 'SepalWidthCm', 'PetalLengthCm', 'PetalWidthCm']) 
+selected_y_var = st.selectbox('What about the y?', 
+  ['SepalLengthCm', 'SepalWidthCm', 'PetalLengthCm', 'PetalWidthCm']) 
+
+# Import Data
+st.title("Iris flower Classifier") 
+Iris = pd.read_csv('C:/Users/Appu/Desktop/Streamlit Test/Iris.csv')
+
+sns.set_style('darkgrid')
+markers = {"Iris-setosa": "X", "Iris-versicolor": "s", "Iris-virginica":'o'}
+fig, ax = plt.subplots() 
+ax = sns.scatterplot(data = Iris, x = selected_x_var, 
+  y = selected_y_var, hue = 'Species', markers = markers,
+  style = 'Species') 
+plt.xlabel(selected_x_var) 
+plt.ylabel(selected_y_var) 
+plt.title("Scatterplot of Palmer's Penguins") 
+st.pyplot(fig) 
